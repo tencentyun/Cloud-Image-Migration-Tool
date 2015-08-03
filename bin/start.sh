e0a6667eb9fdf028e3c57db87597b1781c93b300
Jamis <hoojamis@gmail.com>
2015-08-03 18:06:34 +0800

    add start bash script

---
+++ bin/start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+###############################################################################
+ #  Project: 
+ #  Filename: start.sh 
+ #  Version: 1.0
+ #  Author: Jamis Hoo
+ #  E-mail: hoojamis@gmail.com
+ #  Date: Aug  3, 2015
+ #  Time: 16:51:00
+ #  Description: 
+###############################################################################
+
+SBIN_PATH="../usr/sbin/"
+LIB_PATH="../usr/lib/"
+CONF_PATH="../conf/config.ini"
+
+function get_abs_path() {
+    (
+    cd $(dirname $1)
+    echo $PWD/$(basename $1)
+    )
+}
+
+LIB_PATH=$(get_abs_path $LIB_PATH)
+CONF_PATH=$(get_abs_path $CONF_PATH)
+
+
+python ${SBIN_PATH}/traverse_dir.py $LIB_PATH $CONF_PATH
