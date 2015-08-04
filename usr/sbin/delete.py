b86a8f01426fcff72ccb78660acdfe0e9728cc7c
Jamis <hoojamis@gmail.com>
2015-08-04 15:32:20 +0800

    add other space

--- usr/sbin/delete.py
+++ usr/sbin/delete.py
@@ -5,21 +5,41 @@ sys.path.insert(0, "../lib/")
 
 import tencentyun
 
-appid = "10000037"
-secret_id = "AKIDpoKBfMK7aYcYNlqxnEtYA1ajAqji2P7T"
-secret_key = "P4FewbltIpGeAbwgdrG6eghMUVlpmjIe"
-bucket = "bucket"
+#appid = "10000037"
+#secret_id = "AKIDpoKBfMK7aYcYNlqxnEtYA1ajAqji2P7T"
+#secret_key = "P4FewbltIpGeAbwgdrG6eghMUVlpmjIe"
+#bucket = "bucket"
 
-fileids = [ "111.jpeg", 
-            "download.jpeg", 
-            "images.jpeg", 
-            "tree-247122.jpg", 
-            "dir1/images.jpeg", 
-            "dir1/subdir/UK_Creative_462809583.jpg", 
-            "dir1/subdir/images.jpeg", 
-            "dir1/subdir/tree-247122.jpg", 
-            "dir1/asdas2121sa--++:as123:.jpeg",
-            "ok.jpeg"
+#appid = "10000002"
+#secret_id = "AKIDL5iZVplWMenB5Zrx47X78mnCM3F5xDbC"
+#secret_key = "Lraz7n2vNcyW3tiP646xYdfr5KBV4YAv"
+#bucket = "test1"
+
+
+appid = "10001818"
+secret_id = "AKID1Q25kCKsgs5ZTlGsdzXi0z394LptiQQS"
+secret_key = "tQSzSEj6hvc1e43X5ejExIuG9lihGcdR"
+bucket = "tencentyun"
+
+
+
+fileids = [ 
+
+"1.jpeg",
+"2.jpeg",
+"3.jpeg",
+"4411111.jpg",
+"68asidjui.jpg",
+"8099.jpeg",
+
+"1",
+"2",
+"3",
+"4411111",
+"68asidjui",
+"8099"
+
+
           ]
 image_obj = tencentyun.ImageV2(appid, secret_id, secret_key)
 
