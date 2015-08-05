9d1a45dfa59c6377cb8af85bf16de1e7aa81e4a2
Jamis <hoojamis@gmail.com>
2015-08-05 16:03:05 +0800

    can handle keyboard interrupt

--- usr/sbin/traverse_dir.py
+++ usr/sbin/traverse_dir.py
@@ -12,7 +12,8 @@
 ###############################################################################
 
 from __future__ import print_function
-from multiprocessing import Queue
+import Queue
+import multiprocessing
 import re
 import os
 import random
@@ -49,35 +50,39 @@ def traverse(config, log_path):
     
     image_root_path = os.path.abspath(os.path.expanduser(config["local"]["local.image_root_path"]))
 
-    message_queue = Queue()
-    
-    num_processes = int(config["toolconfig"]["concurrency"])
-    job_queue = JobQueue(
-                         num_processes,
-                         config["appinfo"]["appinfo.appid"],
-                         config["appinfo"]["appinfo.bucket"],
-                         config["appinfo"]["appinfo.secretid"],
-                         config["appinfo"]["appinfo.secretkey"],
-                         message_queue,
-                        )
-     
-    # traverse dir
-    for dirpath, dirs, files in os.walk(image_root_path):
-        for filename in files:
-            if filename_pattern and not filename_pattern.match(filename):
-                continue
-            full_name = os.path.join(dirpath, filename)
-            fileid = full_name[len(image_root_path) + 1: ]
-            # fileid = str(random.randrange(0, 999999999999999999)) + full_name[len(image_root_path) + 1: full_name.find(".")]
-            # print(full_name, ":", fileid)
+    try:
+        message_queue = multiprocessing.Queue()
+        
+        num_processes = int(config["toolconfig"]["concurrency"])
+        job_queue = JobQueue(
+                             num_processes,
+                             config["appinfo"]["appinfo.appid"],
+                             config["appinfo"]["appinfo.bucket"],
+                             config["appinfo"]["appinfo.secretid"],
+                             config["appinfo"]["appinfo.secretkey"],
+                             message_queue,
+                            )
+         
+        # traverse dir
+        for dirpath, dirs, files in os.walk(image_root_path):
+            for filename in files:
+                if filename_pattern and not filename_pattern.match(filename):
+                    continue
+                full_name = os.path.join(dirpath, filename)
+                fileid = full_name[len(image_root_path) + 1: ]
+                # fileid = str(random.randrange(0, 999999999999999999)) + full_name[len(image_root_path) + 1: full_name.find(".")]
+                # print(full_name, ":", fileid)
 
-            job_queue.inqueue(0, full_name, fileid)
-    
-    # Queue is FIFO, so put finish flags after all jobs
-    job_queue.inqueue_finish_flags()
-     
-    num_finished = 0
-    with open(os.path.join(log_path, "stdout"), "w") as stdout, open(os.path.join(log_path, "stderr"), "w") as stderr:
+                job_queue.inqueue(0, full_name, fileid)
+        
+        # Queue is FIFO, so put finish flags after all jobs
+        job_queue.inqueue_finish_flags()
+         
+        num_finished = 0
+
+        stdout = open(os.path.join(log_path, "stdout"), "w")
+        stderr = open(os.path.join(log_path, "stderr"), "w")
+        # receive message from child processes, write to log
         while True:
             message = message_queue.get()
             
@@ -96,5 +101,32 @@ def traverse(config, log_path):
 
         job_queue.finish()
         stdout.write("%s: master process finished \n" % time.asctime())
+    except KeyboardInterrupt:
+        if "job_queue" in locals():
+            job_queue.stop()
 
+
+            while True:
+                try:
+                    message = message_queue.get_nowait()
+                    
+                    # success
+                    if message[0] == 0 and "stdout" in locals():
+                        stdout.write("%s: %s\n" % (time.asctime(), message))
+                    # failure
+                    elif message[0] == 1 and "stderr" in locals():
+                        stderr.write("%s: %s\n" % (time.asctime(), message))
+                    # job finish
+                    elif message[0] == 2 and "stdout" in locals():
+                        num_finished += 1
+                        stdout.write("%s: %d of %d processes interrupted \n" % (time.asctime(), num_finished, num_processes))
+                except Queue.Empty:
+                    break
+            if "stdout" in locals():
+                stdout.write("%s: master process interrupted \n" % time.asctime())
+    finally:
+        if "stdout" in locals():
+            stdout.close()
+        if "stderr" in locals():
+            stderr.close()
 
