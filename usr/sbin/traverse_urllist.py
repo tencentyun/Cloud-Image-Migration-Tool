720282b8d41b2baea32c2e0a75b19eb33ee0b3f6
Jamis <hoojamis@gmail.com>
2015-08-05 16:21:44 +0800

    add support for termination

--- usr/sbin/traverse_urllist.py
+++ usr/sbin/traverse_urllist.py
@@ -14,11 +14,8 @@
 from multiprocessing import Queue
 
 import os
-import time
-import random
 
-def traverse(config, log_path):
-    from job_queue import JobQueue
+def traverse(config, log_path, job_queue):
     import urlparse
 
     mandatory_options = [ 
@@ -42,52 +39,14 @@ def traverse(config, log_path):
 
     urllist_root_path = os.path.abspath(os.path.expanduser(config["urllist"]["url.url_list_file_path"]))
 
-    message_queue = Queue()
-    
-    num_processes = int(config["toolconfig"]["concurrency"])
-    job_queue = JobQueue(
-                         int(config["toolconfig"]["concurrency"]),
-                         config["appinfo"]["appinfo.appid"],
-                         config["appinfo"]["appinfo.bucket"],
-                         config["appinfo"]["appinfo.secretid"],
-                         config["appinfo"]["appinfo.secretkey"],
-                         message_queue,
-                        )
 
     # treverse list
     with open(urllist_root_path) as f:
         for url in f:
             fileid = urlparse.urlparse(url).path
             if len(fileid) and fileid[0] == '/':
-                # TODO: for now fileid cannot contains . /
                 fileid = fileid[1: ]
-                #fileid = str(random.randrange(0, 999999999999999999999999999999999999999999))
-            #print(url)
-            #print(fileid)
             job_queue.inqueue(2, url, fileid)
 
-    # Queue is FIFO, so put finish flags after all jobs
-    job_queue.inqueue_finish_flags()
-     
-    num_finished = 0
-    with open(os.path.join(log_path, "stdout"), "w") as stdout, open(os.path.join(log_path, "stderr"), "w") as stderr:
-        while True:
-            message = message_queue.get()
-            
-            # success
-            if message[0] == 0:
-                stdout.write("%s: %s\n" % (time.asctime(), message))
-            # failure
-            elif message[0] == 1:
-                stderr.write("%s: %s\n" % (time.asctime(), message))
-            # job finish
-            elif message[0] == 2:
-                num_finished += 1
-                stdout.write("%s: %d of %d processes finished \n" % (time.asctime(), num_finished, num_processes))
-                if num_finished == num_processes:
-                    break
-
-        job_queue.finish()
-        stdout.write("%s: master process finished \n" % time.asctime())
 
 
