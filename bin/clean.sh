cabf7e9df6ae98ebbfe8fc22f74f7840ecd7bb3c
Jamis <hoojamis@gmail.com>
2015-08-05 17:49:26 +0800

    add clean script

---
+++ bin/clean.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+###############################################################################
+ #  Copyright (c) 2015 Jamis Hoo
+ #  Project: 
+ #  Filename: clean.sh 
+ #  Version: 1.0
+ #  Author: Jamis Hoo
+ #  E-mail: hoojamis@gmail.com
+ #  Date: Aug  5, 2015
+ #  Time: 17:46:20
+ #  Description: 
+###############################################################################
+
+LOG_PATH="../log/"
+
+function get_abs_path() {
+    (
+    cd $(dirname $1)
+    echo $PWD/$(basename $1)
+    )
+}
+
+LOG_PATH=$(get_abs_path $LOG_PATH)
+
+
+rm -f ${LOG_PATH}/pid
+rm -f ${LOG_PATH}/stderr
+rm -f ${LOG_PATH}/stdout
