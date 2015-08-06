61ccaff5fc3520fe1396f217c68995a22217be3e
Jamis <hoojamis@gmail.com>
2015-08-06 10:35:19 +0800

    add state monitoring script

---
+++ bin/stat.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+###############################################################################
+ #  Copyright (c) 2015 Jamis Hoo
+ #  Distributed under the MIT license 
+ #  (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ #  
+ #  Project: 
+ #  Filename: stat.sh 
+ #  Version: 1.0
+ #  Author: Jamis Hoo
+ #  E-mail: hoojamis@gmail.com
+ #  Date: Aug  6, 2015
+ #  Time: 10:31:47
+ #  Description: 
+###############################################################################
+
+LOG_PATH="../log/"
+
+function get_abs_path() {
+    (
+    cd $(dirname $1)
+    echo $PWD/$(basename $1)
+    )
+}
+
+LOG_PATH=$(get_abs_path $LOG_PATH)
+
+tail -f ${LOG_PATH}/state
