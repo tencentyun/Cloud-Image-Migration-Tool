624f45e7cdcc2ddf916d327e699941b976c8121b
Jamis <hoojamis@gmail.com>
2015-09-08 00:00:54 +0800

    upload.sh will call submit.sh if necessary

--- bin/submit.sh
+++ bin/submit.sh
@@ -2,4 +2,5 @@
 
 . $(dirname $0)/env.sh
 
+echo "submit.sh is running... "
 python ${SBIN_PATH}/main.py $LIB_PATH $CONF_PATH $LOG_PATH submit
--- bin/upload.sh
+++ bin/upload.sh
@@ -2,4 +2,17 @@
 
 . $(dirname $0)/env.sh
 
+if [ ! -f ${LOG_PATH}/jobs.db ]; then
+    sh submit.sh
+fi
+
+db_is_consistent=$(sqlite3 ${LOG_PATH}/jobs.db 'SELECT (SELECT COUNT(*) FROM jobs) = (SELECT value FROM metadata WHERE key = "submitted");')
+if [ $db_is_consistent != "1" ]; then
+    echo "It seems that jobs database is in inconsistent state. "
+    echo "Please run clean.sh to delete all jobs first. "
+    exit 1
+fi
+
+
+echo "Start uploading..."
 python ${SBIN_PATH}/main.py $LIB_PATH $CONF_PATH $LOG_PATH upload
