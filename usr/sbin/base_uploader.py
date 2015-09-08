03b154123f57d3f4c446c8fa50bf4ab93145c612
Jamis <hoojamis@gmail.com>
2015-09-08 20:21:58 +0800

    add doc

--- usr/sbin/base_uploader.py
+++ usr/sbin/base_uploader.py
@@ -11,21 +11,48 @@
  #  E-mail: hoojamis@gmail.com
  #  Date: Sep  7, 2015
  #  Time: 14:29:44
- #  Description: base class of uploader
 ###############################################################################
 
 import abc
 
 class BaseUploader(object):
+    """
+    Abstract base class of uploader.
+    This module is only used in the precedure of uploading.
+    The abstract interface upload() accepts a job, uploads it somewhere and returns log.
+
+    Attributes:
+        mandatory_options: Configuration options required by this class. This is
+            a list of tuples each of which contains two strings, section name and 
+            property name, both of which are case-insensitive.
+
+        config: Copy of configuration
+    """
+
     __metaclass__ = abc.ABCMeta
 
     mandatory_options = []
 
     def __init__(self, config):
+        """
+        Initialize attributes. 
+        """
         self.config = config
 
     @staticmethod
     def check_config(config):
+        """
+        Check whether all required options are provided. 
+        Also check the validity of some options.
+
+        Args:
+            config: configuration dict
+
+        Returns:
+            Returns string containing error message if there are some errors.
+            Returns none otherwise.
+        """
+     
         for section, option in BaseUploader.mandatory_options:
             if section not in config or option not in config[section]:
                 return "Error: Option %s.%s is required. " % (section, option)
@@ -33,10 +60,24 @@ class BaseUploader(object):
     @abc.abstractmethod
     def upload(self, job):
         """
-        type job: (fileid, source)
-        rtype: (new fileid, status, log)
+        Interface which should be implemented in derived class.
 
-        do not throw any exception
+        Args:
+            job: a tuple of (fileid, source)
+                fileid: string, file id of this job
+                source: undefined type, data to be uploaded, could be a filename 
+                    or binary data or in any other forms
+
+        Returns: 
+            log: a tuple of (fileid, status, log)
+                fileid: string, new file id of the job, could be same or different 
+                    with that in argument
+                status: integer, new status of the job
+                log: log in string
+
+        Raises:
+            DO NOT raise any exception in this function.
+
         """
         pass
 
